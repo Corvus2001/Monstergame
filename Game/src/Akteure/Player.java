f5ee447cfdf953ce5deb3b419285183614562543
Corvus <corvin.jaenike@student.htw-berlin.de>
2023-12-23 16:28:45 +0100

    Attribue der PlayerKlasse und Anfänge der Menue Klass

---
+++ Game/src/Akteure/Player.java
@@ -0,0 +1,20 @@
+package Akteure;
+
+public class Player {
+
+    //Attribue
+    double playerlevel;
+    double playerHealthpoints;
+
+    String playerName;
+    int playerAge;
+     String playerMotto;
+
+     //Konstruktor
+    public Player(){
+        this.playerlevel=playerlevel;
+        this.playerHealthpoints=playerHealthpoints;
+        this.playerName=playerName;
+        this.playerAge=playerAge;
+        this.playerMotto=playerMotto;}
+    }
---
+++ Game/src/Menü/Menue.java
@@ -0,0 +1,8 @@
+package Menü;
+
+
+public class Menue {
+    private String[] MenueOptions = {"Start new Game", "Continue Game","Save Game","Quit Game"};
+    
+
+}
